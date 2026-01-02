e005463b3300dd61d4a9faa01ea190af1e620104
diego vega <da-vce@hotmail.com>
2026-01-01 23:04:03 -0500

    Use 'FAILURE' status and remove console log

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,13 +1,12 @@
-// import passport from "passport";
+import passport from "../config/passport.js";
 
-// export const authenticateJWT = (req, res, next) => {
-//   passport.authenticate('jwt', function (error, user, info){
-//     if(error) return next(error);
-//     console.log(user);
-//     if(!user) return res.status(403).json({
-//       status: 'FAILED',
-//       message: 'User not found'
-//     });
-//     next();
-//   })
-// }
+export const authenticateJWT = (req, res, next) => {
+  passport.authenticate('jwt', function (error, user, info) {
+    if (error) return next(error);
+    if (!user) return res.status(403).json({
+      status: 'FAILURE',
+      message: 'User not found'
+    });
+    next();
+  })(req, res, next);
+}
