ca3825f28f77aab5a0b2538ebd9b0b77d7bbfc83
diego vega <da-vce@hotmail.com>
2026-01-03 13:44:44 -0500

    Use try/catch for logIn

--- src/routes/logIn.js
+++ src/routes/logIn.js
@@ -5,15 +5,19 @@ import jwt from 'jsonwebtoken'
 const router = express.Router();
 
 router.post('/', async function (req, res, next) {
-  let dbQueryRes = await pool.query('SELECT * FROM users WHERE email = $1', [req.body.email])
+  try {
+    let dbQueryRes = await pool.query('SELECT * FROM users WHERE email = $1', [req.body.email])
 
-  if (!dbQueryRes.rowCount) return res.status(404).json({ status: 'ERROR', message: 'User not found' })
+    if (!dbQueryRes.rowCount) return res.status(404).json({ status: 'ERROR', message: 'User not found' })
 
-  const user = dbQueryRes.rows[0]
+    const user = dbQueryRes.rows[0]
 
-  const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' })
+    const token = jwt.sign({ id: user.id, email: user.email }, process.env.JWT_SECRET, { expiresIn: '1h' })
 
-  return res.status(200).json({ status: 'OK', message: 'Login successful', data: { id: user.id, email: user.email, token: token } })
+    return res.status(200).json({ status: 'OK', message: 'Login successful', data: { id: user.id, email: user.email, token: token } })
+  } catch (error) {
+    next(error);
+  }
 })
 
 export default router;
