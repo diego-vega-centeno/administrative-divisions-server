b325e687ce5314c07151c3cf47987d5df2c3f674
diego vega <da-vce@hotmail.com>
2026-01-06 19:55:25 -0500

    Implement google oauth and jwt token
    * Use passport-google-oauth20
    * Implement goolge passport config
    * Add router for /auth/google and /auth/google/callback

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,6 +1,7 @@
 import passport from 'passport'
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
 import pool from '../config/db.js'
+import GoogleStrategy from 'passport-google-oauth20';
 
 const opts = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
@@ -18,5 +19,31 @@ passport.use('jwt', new JwtStrategy(opts, async function (payload, done) {
     return done(error);
   }
 }));
+
+passport.use('google', new GoogleStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  callbackURL: "/auth/google/callback"
+},
+  async function (accessToken, refreshToken, profile, done) {
+    try {
+      // find user
+      const user = await pool.query('SELECT * FROM users WHERE oauth_id = $1', [profile.id]);
+
+      if (!user.rowCount) {
+        // create new user
+        const newUser = await pool.query('INSERT INTO users (email, name, oauth_id) VALUES ($1, $2, $3) RETURNING *',
+          [profile.emails[0].value, profile.displayName, profile.id]
+        );
+        return done(null, newUser.rows[0]);
+      }
+
+      return done(null, user.rows[0]);
+    }
+    catch (error) {
+      return done(error)
+    }
+  }
+));
 
 export default passport;
