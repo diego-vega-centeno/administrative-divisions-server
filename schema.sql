98075a7baab4586da08c54f53e7e0262fa988b1b
diego vega <da-vce@hotmail.com>
2026-01-07 18:23:56 -0500

    Change schema column types

--- schema.sql
+++ schema.sql
@@ -1,9 +1,9 @@
 -- Create users table
 CREATE TABLE IF NOT EXISTS users (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  name VARCHAR(255) UNIQUE NOT NULL,
-  email VARCHAR(255) UNIQUE NOT NULL,
-  oauth_id VARCHAR(255) UNIQUE NOT NULL,
+  name VARCHAR(100) UNIQUE NOT NULL,
+  email TEXT UNIQUE NOT NULL,
+  oauth_id TEXT UNIQUE NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
@@ -11,8 +11,8 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS favorites (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-  osm_relation_id VARCHAR(255) NOT NULL,
-  name VARCHAR(255) NOT NULL,
+  osm_relation_id VARCHAR(100) NOT NULL,
+  name VARCHAR(100) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   UNIQUE(user_id, osm_relation_id)
 );
