e95acac66fb4fcb7c5a6acb3bbb76ba78ab39e87
cocobongo <da-vce@hotmail.com>
2026-01-18 17:08:34 -0500

    refactor: Create a userRoute.js - Use '/me' and '/logout' paths

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -3,8 +3,13 @@ import { authenticateJWT } from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.get('/', authenticateJWT, (req, res) => {
+router.get('/me', authenticateJWT, (req, res) => {
   res.status(200).json({ status: 'OK', data: req.user });
+});
+
+router.get('/logout', authenticateJWT, (req, res) => {
+  res.clearCookie('jwt');
+  res.json({ status: 'OK' })
 });
 
 export default router;
