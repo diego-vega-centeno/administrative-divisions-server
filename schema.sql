e8c160bfb4f6bfaafff5791e0d45dfd6664f380b
cocobongo <da-vce@hotmail.com>
2026-01-22 11:51:08 -0500

    refactor: Remove favorites schema

--- schema.sql
+++ schema.sql
@@ -7,17 +7,6 @@ CREATE TABLE IF NOT EXISTS users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Create favorites table
-CREATE TABLE IF NOT EXISTS favorites (
-  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-  user_id UUID REFERENCES users(id) ON DELETE CASCADE,
-  osm_relation_id VARCHAR(100) NOT NULL,
-  osm_relation_name VARCHAR(100) NOT NULL,
-  text VARCHAR(100) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  UNIQUE(user_id, osm_relation_id)
-);
-
 -- Create layers table
 CREATE TABLE IF NOT EXISTS layers (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
