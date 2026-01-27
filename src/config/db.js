6e2a2f7689effac5f800a655534f347f29f70f83
diego vega <da-vce@hotmail.com>
2026-01-27 16:05:54 -0500

    feat: Add supabase postgre connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,15 @@ import pg from 'pg'
 const testEnvs = ['test', 'development'];
 
 const pool = new pg.Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: testEnvs.includes(process.env.NODE_ENV) ? process.env.TEST_DATABASE : process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
+  // if it has value this key will override other connection keys
+  connectionString: testEnvs.includes(process.env.NODE_ENV) ?
+    undefined :
+    process.env.SUPABASE_POSTGRE_DIRECT_URI,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.TEST_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 
 export default pool;
