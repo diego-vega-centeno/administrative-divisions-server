118440f3aa55fde91e06a28b4270530e4ef509b6
diego vega <da-vce@hotmail.com>
2026-01-30 00:15:35 -0500

    refactor: Improve column names returned

--- src/models/layerReads.js
+++ src/models/layerReads.js
@@ -1,14 +1,14 @@
 import pool from "../config/db.js";
 
-async function getUserLayersRelations(userId){
+async function getUserLayersRelations(userId) {
   const response = await pool.query(`
-    SELECT ly.id, ly.title, lr.osm_relation_id, lr.osm_relation_name 
+    SELECT lr.id as id, ly.id as layer_id, ly.title as layer_title, lr.osm_relation_id, lr.osm_relation_name 
     FROM layers as ly
     JOIN users ON users.id = ly.user_id
     JOIN layer_relations as lr ON lr.layer_id = ly.id
     WHERE users.id = $1`, [userId]);
-    console.log(response.rows);
-    return response.rows;
+
+  return response.rows;
 }
 
-export {getUserLayersRelations}
+export { getUserLayersRelations }
