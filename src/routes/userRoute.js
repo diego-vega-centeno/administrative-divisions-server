4481bed83941a0079d8dafd17d75291df62fd0b7
diego vega <da-vce@hotmail.com>
2026-02-01 12:13:01 -0500

    fix: Use same attributes to work on mobile

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -4,11 +4,15 @@ import { authenticateJWT } from "../middleware/auth.js";
 const router = express.Router();
 
 router.get('/me', authenticateJWT, (req, res) => {
-  res.status(200).json({data: req.user });
+  res.status(200).json({ data: req.user });
 });
 
 router.get('/logout', authenticateJWT, (req, res) => {
-  res.clearCookie('jwt');
+  res.clearCookie('jwt', {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+  });
   res.sendStatus(201);
 });
 
