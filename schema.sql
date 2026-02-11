bc8542300e885f285984d982a4936b392a452eca
diego vega <da-vce@hotmail.com>
2026-02-10 21:30:56 -0500

    feat: new schema to include admin_level and parents with osm_relation_id index

--- schema.sql
+++ schema.sql
@@ -21,7 +21,16 @@ CREATE TABLE IF NOT EXISTS layer_relations (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   layer_id UUID REFERENCES layers(id) ON DELETE CASCADE,
   osm_relation_id VARCHAR(100) NOT NULL,
-  osm_relation_name VARCHAR(100) NOT NULL,
+  osm_relation_name VARCHAR(255) NOT NULL,
+  parents TEXT NOT NULL,
+  admin_level VARCHAR(100) NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   UNIQUE(layer_id, osm_relation_id)
-)
+);
+
+-- Layer_relations indexes
+
+-- this one is automatically created by the UNIQUE constraint
+-- CREATE INDEX idx_layer_relations_layer_id ON layer_relations(layer_id);
+
+CREATE INDEX idx_layer_relations_osm_id ON layer_relations(osm_relation_id);
