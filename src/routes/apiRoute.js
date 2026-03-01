ff8e330d21254c99c4b0946527103b4a43397525
diego vega <da-vce@hotmail.com>
2026-03-01 00:10:54 -0500

    refactor: remove authentication from API

--- src/routes/apiRoute.js
+++ src/routes/apiRoute.js
@@ -1,10 +1,9 @@
 import express from 'express';
 import osmRelsByCntr from '../data/rels_index_api_by_cntr.json' with { type: 'json' };
-import { authenticateJWT } from '../middleware/auth.js';
 
 const router = express.Router();
 
-router.get('/countries/:countryId', authenticateJWT, async function (req, res, next) {
+router.get('/countries/:countryId', async function (req, res, next) {
   const { countryId } = req.params;
   const levels = req.query.levels?.split(',') ?? null;
   
@@ -16,7 +15,7 @@ router.get('/countries/:countryId', authenticateJWT, async function (req, res, n
   if (levels) {
     resultRels = resultRels.filter(rel => levels.includes(rel.admin_level));
   }
-  return res.status(200).json({data: resultRels })
+  return res.status(200).json(resultRels)
 });
 
 export default router;
