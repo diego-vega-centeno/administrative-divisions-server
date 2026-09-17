8ead701588e232f537fdae034fae4f15b20818fa
diego vega <da-vce@hotmail.com>
2026-09-17 18:09:53 -0500

    feat: assert env variables

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,17 +1,17 @@
-import pg from 'pg'
+import pg from "pg";
 
-const testEnvs = ['test', 'development'];
+const testEnvs = ["test", "development"];
 
 const pool = new pg.Pool({
   // if it has value this key will override other connection keys
-  connectionString: testEnvs.includes(process.env.NODE_ENV) ?
-    undefined :
-    process.env.SUPABASE_POSTGRE_DIRECT_URI,
+  connectionString: testEnvs.includes(process.env.NODE_ENV!)
+    ? undefined
+    : process.env.SUPABASE_POSTGRE_DIRECT_URI,
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.TEST_DATABASE,
   password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
+  port: process.env.PG_PORT ? Number(process.env.PG_PORT) : undefined,
 });
 
 export default pool;
