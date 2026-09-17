cbc211cade6368f30985f1b61c1579f0da4d535c
diego vega <da-vce@hotmail.com>
2026-09-17 18:10:50 -0500

    feat: use named import to fix type issues

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -1,112 +1,138 @@
-import passport from 'passport'
-import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt'
-import pool from './db.js'
-import GoogleStrategy from 'passport-google-oauth20';
-import OAuth2Strategy from 'passport-oauth2';
+import passport from "passport";
+import { Strategy as JwtStrategy, ExtractJwt } from "passport-jwt";
+import pool from "./db.js";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import OAuth2Strategy from "passport-oauth2";
+import type { Request } from "express";
 
-const cookieExtractor = (req) => {
+const cookieExtractor = (req: Request) => {
   // all token available
   let token;
   if (req && req.cookies) {
-    token = req.cookies['jwt'];
+    token = req.cookies["jwt"];
   }
   return token;
-}
+};
 
 const opts = {
   jwtFromRequest: cookieExtractor,
-  secretOrKey: process.env.JWT_SECRET,
-  ignoreExpiration: false
-}
+  secretOrKey: process.env.JWT_SECRET!,
+  ignoreExpiration: false,
+};
 
-passport.use('jwt', new JwtStrategy(opts, async function (payload, done) {
-  try {
-    const userResponse = await pool.query(
-      'SELECT * FROM users WHERE id=$1 LIMIT 1',
-      [payload.id]
-    );
-    if (!userResponse.rowCount) return done(null, false, { message: 'User not found' });
-
-    return done(null, userResponse.rows[0]);
-  } catch (error) {
-    return done(error);
-  }
-}));
-
-passport.use('google', new GoogleStrategy({
-  clientID: process.env.GOOGLE_CLIENT_ID,
-  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.NODE_ENV === 'development' ?
-    process.env.CALLBACK_DEV_GOOGLE_URL : process.env.CALLBACK_PROD_GOOGLE_URL,
-},
-  async function (accessToken, refreshToken, profile, done) {
+passport.use(
+  "jwt",
+  new JwtStrategy(opts, async function (payload, done) {
     try {
-      // find user
-      let user;
-      user = await pool.query(
-        'SELECT * FROM users WHERE oauth_id = $1',
-        ['google-' + profile.id]
+      const userResponse = await pool.query(
+        "SELECT * FROM users WHERE id=$1 LIMIT 1",
+        [payload.id],
       );
+      if (!userResponse.rowCount)
+        return done(null, false, { message: "User not found" });
 
-      if (!user.rowCount) {
-        // create new user
-        user = await pool.query(
-          'INSERT INTO users (name, oauth_id) VALUES ($1, $2) RETURNING *',
-          [profile.displayName, 'google-' + profile.id]
-        );
+      return done(null, userResponse.rows[0]);
+    } catch (error) {
+      return done(error);
+    }
+  }),
+);
+
+passport.use(
+  "google",
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      callbackURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.CALLBACK_DEV_GOOGLE_URL
+          : process.env.CALLBACK_PROD_GOOGLE_URL,
+    },
+    async function (accessToken, refreshToken, profile, done) {
+      try {
+        // find user
+        let user;
+        user = await pool.query("SELECT * FROM users WHERE oauth_id = $1", [
+          "google-" + profile.id,
+        ]);
+
+        if (!user.rowCount) {
+          // create new user
+          user = await pool.query(
+            "INSERT INTO users (name, oauth_id) VALUES ($1, $2) RETURNING *",
+            [profile.displayName, "google-" + profile.id],
+          );
+        }
+
+        return done(null, user.rows[0]);
+      } catch (error) {
+        return done(error);
       }
+    },
+  ),
+);
 
-      return done(null, user.rows[0]);
-    }
-    catch (error) {
-      return done(error)
-    }
-  }
-));
+type OSMUserResponse = {
+  user: {
+    id: string;
+    display_name: string;
+  };
+};
 
-
-passport.use(new OAuth2Strategy({
-  authorizationURL: 'https://www.openstreetmap.org/oauth2/authorize?' +
-    'response_type=code&' +
-    `client_id=${process.env.OSM_CLIENT_ID}&` +
-    `redirect_uri=${process.env.CALLBACK_DEV_OSM_URL}&` +
-    `scope=read_prefs`,
-  tokenURL: 'https://www.openstreetmap.org/oauth2/token',
-  clientID: process.env.OSM_CLIENT_ID,
-  clientSecret: process.env.OSM_CLIENT_SECRET,
-  callbackURL: process.env.NODE_ENV === 'development' ?
-    process.env.CALLBACK_DEV_OSM_URL : process.env.CALLBACK_PROD_OSM_URL,
-  scope: ['read_prefs']
-},
-  async function (accessToken, refreshToken, profile, done) {
-    try {
-      const res = await fetch('https://api.openstreetmap.org/api/0.6/user/details.json', {
-        headers: { Authorization: `Bearer ${accessToken}` }
-      });
-      const userData = await res.json();
-
-      // find user
-      let user;
-      user = await pool.query(
-        'SELECT * FROM users WHERE oauth_id = $1',
-        ['osm-' + userData.user.id]
-      );
-
-      if (!user.rowCount) {
-        // create new user
-        user = await pool.query(
-          'INSERT INTO users (name, oauth_id) VALUES ($1, $2) RETURNING *',
-          [userData.user.display_name, 'osm-' + userData.user.id]
+passport.use(
+  new OAuth2Strategy(
+    {
+      authorizationURL:
+        "https://www.openstreetmap.org/oauth2/authorize?" +
+        "response_type=code&" +
+        `client_id=${process.env.OSM_CLIENT_ID!}&` +
+        `redirect_uri=${process.env.CALLBACK_DEV_OSM_URL!}&` +
+        `scope=read_prefs`,
+      tokenURL: "https://www.openstreetmap.org/oauth2/token",
+      clientID: process.env.OSM_CLIENT_ID!,
+      clientSecret: process.env.OSM_CLIENT_SECRET!,
+      callbackURL:
+        process.env.NODE_ENV === "development"
+          ? process.env.CALLBACK_DEV_OSM_URL
+          : process.env.CALLBACK_PROD_OSM_URL,
+      scope: ["read_prefs"],
+    },
+    async function (
+      accessToken: string,
+      refreshToken: string,
+      profile: any,
+      done: any,
+    ) {
+      try {
+        const res = await fetch(
+          "https://api.openstreetmap.org/api/0.6/user/details.json",
+          {
+            headers: { Authorization: `Bearer ${accessToken}` },
+          },
         );
+        const userData = (await res.json()) as OSMUserResponse;
+
+        // find user
+        let user;
+        user = await pool.query("SELECT * FROM users WHERE oauth_id = $1", [
+          "osm-" + userData.user.id,
+        ]);
+
+        if (!user.rowCount) {
+          // create new user
+          user = await pool.query(
+            "INSERT INTO users (name, oauth_id) VALUES ($1, $2) RETURNING *",
+            [userData.user.display_name, "osm-" + userData.user.id],
+          );
+        }
+
+        return done(null, user.rows[0]);
+      } catch (error) {
+        return done(error);
       }
-
-      return done(null, user.rows[0]);
-    }
-    catch (error) {
-      return done(error)
-    }
-  }
-));;
-
+    },
+  ),
+);
 
 export default passport;
