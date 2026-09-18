1b4293e90b38cd7ad9bd9d0dd1a223846f3d2763
diego vega <da-vce@hotmail.com>
2026-09-17 20:49:43 -0500

    feat: remove email column

--- src/__tests__/helpers/testAuth.ts
+++ src/__tests__/helpers/testAuth.ts
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 import pool from '../../config/db.js';
 
-async function generateTestToken(user = { name: 'test_user_name', email: 'test_user@test.com', oauthId: 'google-test_user' }) {
+async function generateTestToken(user = { name: 'test_user_name', oauthId: 'google-test_user' }) {
   const userResp = await pool.query(`
-    INSERT INTO users (name, email, oauth_id) 
-    VALUES ($1, $2, $3)
-    RETURNING id`, [user.name, user.email, user.oauthId]
+    INSERT INTO users (name, oauth_id) 
+    VALUES ($1, $2)
+    RETURNING id`, [user.name, user.oauthId]
   );
-  return jwt.sign({ id: userResp.rows[0].id, email: user.email }, process.env.JWT_SECRET!, { expiresIn: '1h' });
+  return jwt.sign({ id: userResp.rows[0].id }, process.env.JWT_SECRET!, { expiresIn: '1h' });
 }
 
 export { generateTestToken }
